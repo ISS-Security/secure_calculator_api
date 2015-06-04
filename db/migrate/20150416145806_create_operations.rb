14705f7fde88a6c5271789319b94776be2f426c9
Soumya Ray <soumya.ray@gmail.com>
2015-06-05 06:00:09 +0800

    Save full operation request in db
    - db/migrate/20150416145806_create_operations.rb
    	- add user_id integer attribute
      - helpers/calc_helper.rb:
    		- save operation to db in #randeom_simple
    		- check user_id in #authenticate_client_from_header

--- db/migrate/20150416145806_create_operations.rb
+++ db/migrate/20150416145806_create_operations.rb
@@ -1,6 +1,7 @@
 class CreateOperations < ActiveRecord::Migration
   def change
     create_table :operations do |t|
+      t.integer :user_id
       t.string :operation
       t.text :encrypted_parameters
       t.string :nonce
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150416145806) do
 
   create_table "operations", force: :cascade do |t|
+    t.integer  "user_id"
     t.string   "operation"
     t.text     "encrypted_parameters"
     t.string   "nonce"
